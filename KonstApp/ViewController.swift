21ce101bc217519e0e10ab9fce7b898d1f753a28
Fanny Erkhammar <fannyerkhammar@fannys-mbp.kista.se.ibm.com>
2018-02-23 14:26:00 +0100

    lade till två knappar som visar olika textrutor (funkar på iPhone 7 men inte 8s)

--- KonstApp/ViewController.swift
+++ KonstApp/ViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: Properties
+    @IBOutlet weak var verkText: UIView!
+    @IBOutlet weak var konstnarText: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Hide the textfields
+        verkText.isHidden = true
+        konstnarText.isHidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //MARK: Actions
+    
+    //show verkText if "Om verket" button is pressed, and hide other text fields
+    @IBAction func showVerkText(_ sender: UIButton) {
+        verkText.isHidden = false
+        konstnarText.isHidden = true
+    }
+    
+     //show konstnarText if "Om konstnären" button is pressed, and hide other text fields
+    @IBAction func showKonstnarText(_ sender: UIButton) {
+        konstnarText.isHidden = false
+        verkText.isHidden = true
+    }
+    
 }
 
