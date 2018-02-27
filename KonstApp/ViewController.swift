ba5d9087c59de8829938124d5fe47b632f771f94
Kristin Rosen <kristinrosen@kristins-mbp.kista.se.ibm.com>
2018-02-27 08:36:56 +0100

    animation åt båda hållen

--- KonstApp/ViewController.swift
+++ KonstApp/ViewController.swift
@@ -34,33 +34,89 @@ class ViewController: UIViewController{
     @IBAction func nextText(_ sender: UISwipeGestureRecognizer) {
         if displayString == nil {
         displayString = infoTexts[0]
+        textView.leftToRightAnimation()
         textView.text = displayString
         } else if displayString == infoTexts[0] {
             displayString = infoTexts[1]
+            textView.leftToRightAnimation()
             textView.text = displayString
         } else if displayString == infoTexts[1] {
             displayString = infoTexts[2]
+            textView.leftToRightAnimation()
             textView.text = displayString
         } else {
             displayString = nil
             textView.text = displayString
+            textView.leftToRightAnimation()
         }
     }
     
     @IBAction func previousText(_ sender: UISwipeGestureRecognizer) {
         if displayString == nil {
             displayString = infoTexts[2]
+            textView.rightToLeftAnimation()
             textView.text = displayString
         } else if displayString == infoTexts[2] {
             displayString = infoTexts[1]
+            textView.rightToLeftAnimation()
             textView.text = displayString
         } else if displayString == infoTexts[1] {
             displayString = infoTexts[0]
+            textView.rightToLeftAnimation()
             textView.text = displayString
         } else {
             displayString = nil
+            textView.rightToLeftAnimation()
             textView.text = displayString
         }
+    }
+    
+}
+
+extension UIView {
+    func leftToRightAnimation(duration: TimeInterval = 0.5, completionDelegate: AnyObject? = nil) {
+        // Create a CATransition object
+        let leftToRightTransition = CATransition()
+        
+        // Set its callback delegate to the completionDelegate that was provided
+        if let delegate: AnyObject = completionDelegate {
+            leftToRightTransition.delegate = delegate as? CAAnimationDelegate
+            
+        }
+        
+        leftToRightTransition.type = kCATransitionPush
+        leftToRightTransition.subtype = kCATransitionFromRight
+        leftToRightTransition.duration = duration
+        leftToRightTransition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        leftToRightTransition.fillMode = kCAFillModeRemoved
+        
+        // Add the animation to the View's layernäm
+        self.layer.add(leftToRightTransition, forKey: "leftToRightTransition")
+        
+    }
+    
+}
+
+extension UIView {
+    func rightToLeftAnimation(duration: TimeInterval = 0.5, completionDelegate: AnyObject? = nil) {
+        // Create a CATransition object
+        let rightToLeftTransition = CATransition()
+        
+        // Set its callback delegate to the completionDelegate that was provided
+        if let delegate: AnyObject = completionDelegate {
+            rightToLeftTransition.delegate = delegate as? CAAnimationDelegate
+            
+        }
+        
+        rightToLeftTransition.type = kCATransitionPush
+        rightToLeftTransition.subtype = kCATransitionFromLeft
+        rightToLeftTransition.duration = duration
+        rightToLeftTransition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        rightToLeftTransition.fillMode = kCAFillModeRemoved
+        
+        // Add the animation to the View's layernäm
+        self.layer.add(rightToLeftTransition, forKey: "rightToLeftTransition")
+        
     }
     
 }
