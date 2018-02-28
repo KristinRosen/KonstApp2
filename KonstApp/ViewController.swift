474fafc35cf8e855edf1f8b38ae5f93a6486faeb
Fanny Erkhammar <fannyerkhammar@fannys-mbp.kista.se.ibm.com>
2018-02-28 10:14:15 +0100

    flyttade in all kod i ViewController.swift + tog bort Extensions.swift

--- KonstApp/ViewController.swift
+++ KonstApp/ViewController.swift
@@ -200,6 +200,51 @@ class ViewController: UIViewController {
     
 }
 
+//Animation
+extension UIView {
+    public func leftToRightAnimation(duration: TimeInterval = 0.5, completionDelegate: AnyObject? = nil) {
+        // Create a CATransition object
+        let leftToRightTransition = CATransition()
+        
+        // Set its callback delegate to the completionDelegate that was provided
+        if let delegate: AnyObject = completionDelegate {
+            leftToRightTransition.delegate = delegate as? CAAnimationDelegate
+            
+        }
+        
+        leftToRightTransition.type = kCATransitionPush
+        leftToRightTransition.subtype = kCATransitionFromRight
+        leftToRightTransition.duration = duration
+        leftToRightTransition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        leftToRightTransition.fillMode = kCAFillModeRemoved
+        
+        // Add the animation to the View's layernäm
+        self.layer.add(leftToRightTransition, forKey: "leftToRightTransition")
+        
+    }
+    
+}
 
-
-
+extension UIView {
+    public func rightToLeftAnimation(duration: TimeInterval = 0.5, completionDelegate: AnyObject? = nil) {
+        // Create a CATransition object
+        let rightToLeftTransition = CATransition()
+        
+        // Set its callback delegate to the completionDelegate that was provided
+        if let delegate: AnyObject = completionDelegate {
+            rightToLeftTransition.delegate = delegate as? CAAnimationDelegate
+            
+        }
+        
+        rightToLeftTransition.type = kCATransitionPush
+        rightToLeftTransition.subtype = kCATransitionFromLeft
+        rightToLeftTransition.duration = duration
+        rightToLeftTransition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        rightToLeftTransition.fillMode = kCAFillModeRemoved
+        
+        // Add the animation to the View's layernäm
+        self.layer.add(rightToLeftTransition, forKey: "rightToLeftTransition")
+        
+    }
+    
+}
