ee39426cd64796da65f156c5a0daf4b9c6d8621c
Fanny Erkhammar <fannyerkhammar@fannys-mbp.kista.se.ibm.com>
2018-03-20 09:49:47 +0100

    lade till table view + konstverktableViewController + konstverkTableViewCell och sen started de galna experimenten...

---
+++ KonstApp/Konstverk.swift
@@ -0,0 +1,26 @@
+//
+//  Konstverk.swift
+//  KonstApp
+//
+//  Created by Fanny Erkhammar on 2018-03-20.
+//  Copyright © 2018 Kristin Rosen and Fanny Erkhammar. All rights reserved.
+//
+
+import UIKit
+
+struct KonstverkData: Decodable {
+    let namn: String
+    let bild: String
+    let texter: [String]
+}
+
+class Konstverk {
+
+//MARK: Properties
+
+    var name: String
+    
+    var image: String
+    
+    
+}
